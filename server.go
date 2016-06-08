e1d4b08bea8c4b3ddb1d50479a1134fde765f6dc
Yuriy Firman <mail@fast-foto.com>
2016-06-08 16:15:40 +0300

    First commit

---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/lux-foto/gphoto"
+	"github.com/lux-foto/movie-server"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/camera/{type}", gphoto.CameraHandler)
+	r.HandleFunc("/video/shot/{shot}/{movie}/", movie_server.VideoShotServer)
+	r.HandleFunc("/video/{name}/", movie_server.VideoMovieServer)
+	log.Println("Listening on 3000")
+	log.Fatal(http.ListenAndServe(":3000", r))
+
+}
+
